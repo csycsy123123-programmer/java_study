1c00f5d48acd21d90afce46f87a2ef6dd9dc0319
csy <a1227556397@163.com>
2024-09-11 16:42:34 +0800

    vscode试验

---
+++ src/List/LinkedList.java
@@ -0,0 +1,39 @@
+package List;
+
+public class TestLinkedList<E> extends AbstractList<E> {
+
+    @Override
+    public void add(E element) {
+        
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
